da5ce52e92efc2085b85d5e61137234a350396f7
ckybi <berloffartem@yandex.ru>
2023-07-21 13:13:15 +0300

    freeze

--- const.js
+++ const.js
@@ -1,6 +1,10 @@
+// команды для бота 
 const commands = `
 /start - Перезапуск бота 
-/help - Помощь с командами `
+/help - Помощь с командами 
+/admin - Информация для админов
+/advert - Информация для рекламодателя
+/info - Техподдержка`
 
 
 module.exports.commands = commands
